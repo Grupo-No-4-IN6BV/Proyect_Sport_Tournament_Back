e06a8ffaf86e41b3c8099044fad22d636dee492c
André Ortega <aortega-2019275@kinal.edu.gt>
2021-06-15 22:25:04 -0600

    rutas team

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -5,5 +5,10 @@ var api = express.Router();
 var mdAuth = require('../middlewares/authenticated');
 var teamController = require('../controllers/team.controller');
 
+api.put('/:id/setTeam', teamController.setTeam);
+api.put('/:idL/updateTeam/:idT', teamController.updateTeam);
+api.put('/:idL/removeTeam/:idT', teamController.removeTeam);
+api.get('/getTeams', teamController.getTeams);
+
 
 module.exports = api;
